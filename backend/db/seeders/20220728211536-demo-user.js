76066769598a38f4bd09f09435d337f60974276a
linyangofmay <linyangofmay@gmail.com>
2022-07-29 15:03:46 -0700

    changeawait

--- backend/db/seeders/20220728211536-demo-user.js
+++ backend/db/seeders/20220728211536-demo-user.js
@@ -11,7 +11,7 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
-     return queryInterface.bulkInsert('Users', [
+     await queryInterface.bulkInsert('Users', [
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
