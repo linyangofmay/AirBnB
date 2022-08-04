e53e4882df1c6be84545e557be009e74f05dfe40
linyangofmay <linyangofmay@gmail.com>
2022-08-03 22:12:59 -0700

    changeorder

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Booking.belongsTo(models.User, { foreignKey: 'userId'});
+      Booking.belongsTo(models.Spot, { foreignKey: 'spotId'})
     }
   }
   Booking.init({
@@ -26,7 +27,9 @@ module.exports = (sequelize, DataTypes) => {
       type:DataTypes.DATE,
       allowNull: false
     },
-    endDate: DataTypes.DATE
+    endDate: {
+      type: DataTypes.DATE
+    }
   }, {
     sequelize,
     modelName: 'Booking',
